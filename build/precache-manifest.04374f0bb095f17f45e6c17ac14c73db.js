6ac267b58e6f4752f026fbec302e72ac26c9cc73
hugnw-ilee <hungquyle123456@gmail.com>
2025-04-10 14:39:34 +0700

    3rd change

---
+++ build/precache-manifest.04374f0bb095f17f45e6c17ac14c73db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93b9c3e02bea690f660d90f0ce1201da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "495db36826346740fb88",
+    "url": "/static/css/2.6cc21fe3.chunk.css"
+  },
+  {
+    "revision": "26d550fadc43dfadd415",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "495db36826346740fb88",
+    "url": "/static/js/2.7bbdc1b7.chunk.js"
+  },
+  {
+    "revision": "fb2cf70c462ba668ffd976a5d27762ac",
+    "url": "/static/js/2.7bbdc1b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "454aca444c7a6351e6ad",
+    "url": "/static/js/3.9a10f4f4.chunk.js"
+  },
+  {
+    "revision": "26d550fadc43dfadd415",
+    "url": "/static/js/main.40ac5811.chunk.js"
+  },
+  {
+    "revision": "1635bfea7e50500a340a",
+    "url": "/static/js/runtime-main.9cae0b71.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -7,148 +7,159 @@ import Button from "react-bootstrap/Button";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
 import ListGroup from "react-bootstrap/ListGroup";
+import axios from "axios";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userInput: "",
+      content: "",
       list: [],
       completedList: [],
     };
   }
 
-  updateInput = (value) => {
-    this.setState({ userInput: value });
+  componentDidMount() {
+    axios.get("http://localhost:8000/api/todos")
+      .then(response => {
+        this.setState({
+          list: response.data.filter(todo => !todo.IsDone),
+          completedList: response.data.filter(todo => todo.IsDone),
+        });
+      })
+      .catch(error => console.error("Lỗi khi tải dữ liệu:", error));
+  }
+
+  updateInput = (e) => {
+    this.setState({ content: e.target.value });
   };
 
   addItem = () => {
-    const { userInput } = this.state;
-    if (!userInput.trim()) return;
+    const { content } = this.state;
+    if (!content.trim()) return;
 
-    this.setState((prevState) => ({
-      list: [
-        ...prevState.list,
-        {
-          id: Date.now(),
-          value: userInput.trim(),
-        },
-      ],
-      userInput: "",
-    }));
+    axios.post("http://localhost:8000/api/todos", { Content: content.trim() })
+      .then(response => {
+        this.setState(prevState => ({
+          list: [...prevState.list, response.data],
+          content: "",
+        }));
+      })
+      .catch(error => console.error("Lỗi khi thêm công việc:", error));
   };
 
   deleteItem = (id) => {
-    this.setState((prevState) => ({
-      list: prevState.list.filter((item) => item.id !== id),
-    }));
+    axios.delete(`http://localhost:8000/api/todos/${id}`)
+      .then(() => {
+        this.setState(prevState => ({
+          list: prevState.list.filter(item => item.ID !== id), 
+          completedList: prevState.completedList.filter(item => item.ID !== id), 
+        }));
+      })
+      .catch(error => console.error("Lỗi khi xóa công việc:", error));
   };
 
   editItem = (id) => {
     const { list } = this.state;
-    const item = list.find((item) => item.id === id);
+    const item = list.find(item => item.ID === id); 
     if (!item) return;
 
-    const editedValue = prompt("Sửa công việc:", item.value);
-    if (editedValue && editedValue.trim()) {
-      this.setState((prevState) => ({
-        list: prevState.list.map((item) =>
-          item.id === id ? { ...item, value: editedValue.trim() } : item
-        ),
-      }));
+    const editedContent = prompt("Sửa công việc:", item.Content); 
+    if (editedContent && editedContent.trim()) {
+      axios.put(`http://localhost:8000/api/todos/${id}`, {
+        Content: editedContent.trim() 
+      })
+        .then(response => {
+          this.setState(prevState => ({
+            list: prevState.list.map(item =>
+              item.ID === id ? { ...item, Content: response.data.Content } : item 
+            ),
+          }));
+        })
+        .catch(error => console.error("Lỗi khi sửa công việc:", error));
     }
   };
 
   completeItem = (id) => {
-    this.setState((prevState) => {
-      const item = prevState.list.find((item) => item.id === id);
-      if (!item) return prevState;
-
-      return {
-        list: prevState.list.filter((item) => item.id !== id),
-        completedList: [...prevState.completedList, item],
-      };
-    });
+    axios.put(`http://localhost:8000/api/todos/${id}/complete`)
+      .then(response => {
+        this.setState(prevState => ({
+          list: prevState.list.filter(item => item.ID !== id), 
+          completedList: [...prevState.completedList, response.data],
+        }));
+      })
+      .catch(error => console.error("Lỗi khi hoàn thành công việc:", error));
   };
 
   render() {
     return (
       <Container>
-        <Row
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            fontSize: "3rem",
-            fontWeight: "bolder",
-          }}
-        >
+        <Row style={{ 
+          display: "flex", 
+          justifyContent: "center", 
+          alignItems: "center", 
+          fontSize: "3rem", 
+          fontWeight: "bolder" 
+        }}>
           TODO LIST
         </Row>
-
         <hr />
         <Row>
           <Col md={{ span: 2 }} className="bg-dark text-white p-3">
             <h5>Công việc đã hoàn thành</h5>
             <ListGroup>
               {this.state.completedList.map((item) => (
-                <ListGroup.Item key={item.id} variant="success">
-                  {item.value}
+                <ListGroup.Item key={item.ID} variant="success"> 
+                  {item.Content} 
                 </ListGroup.Item>
               ))}
             </ListGroup>
           </Col>
-
+          
           <Col md={{ span: 6, offset: 1 }}>
             <InputGroup className="mb-3">
               <FormControl
                 placeholder="Thêm công việc . . . "
                 size="lg"
-                value={this.state.userInput}
-                onChange={(e) => this.updateInput(e.target.value)}
-                aria-label="Thêm gì đó"
-                aria-describedby="basic-addon2"
+                value={this.state.content}
+                onChange={this.updateInput}
+                aria-label="Thêm công việc"
               />
-              <InputGroup>
-                <Button
-                  variant="dark"
-                  className="mt-2"
-                  onClick={this.addItem}
-                >
-                  Thêm
-                </Button>
-              </InputGroup>
+              <Button variant="dark" onClick={this.addItem}>Thêm</Button>
             </InputGroup>
-
+            
             <ListGroup>
               {this.state.list.map((item) => (
-                <ListGroup.Item
-                  key={item.id}
-                  variant="dark"
-                  action
-                  style={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    alignItems: "center",
+                <ListGroup.Item 
+                  key={item.ID} 
+                  variant="dark" 
+                  action 
+                  style={{ 
+                    display: "flex", 
+                    justifyContent: "space-between", 
+                    alignItems: "center" 
                   }}
                 >
-                  {item.value}
+                  {item.Content}
                   <div>
-                    <Button
-                      variant="light"
-                      style={{ marginRight: "10px" }}
-                      onClick={() => this.deleteItem(item.id)}>
+                    <Button 
+                      variant="light" 
+                      style={{ marginRight: "10px" }} 
+                      onClick={() => this.deleteItem(item.ID)}
+                    >
                       Xóa
                     </Button>
-                    <Button
-                      variant="light"
-                      style={{ marginRight: "10px" }}
-                      onClick={() => this.editItem(item.id)}>
+                    <Button 
+                      variant="light" 
+                      style={{ marginRight: "10px" }} 
+                      onClick={() => this.editItem(item.ID)} 
+                    >
                       Sửa
                     </Button>
-                    <Button
-                      variant="light"
-                      onClick={() => this.completeItem(item.id)}>
+                    <Button 
+                      variant="light" 
+                      onClick={() => this.completeItem(item.ID)} 
+                    >
                       Xong
                     </Button>
                   </div>
